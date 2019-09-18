303d59c7cabbd6e745f5e319112bdf4a0bcf5f92
Ramon do Nascimento Pereira <ramonnascimento@brq.com>
2019-09-18 19:01:31 -0300

    adicionado chart Jogadas

--- src/pages/Chart/index.js
+++ src/pages/Chart/index.js
@@ -8,15 +8,13 @@ class Chart extends PureComponent {
 
     state = {
         data :  [
-          { name: '10/01', Pontos: 2 },
-          { name: '12/01', Pontos: 8 },
-          { name: '13/01', Pontos: 5 },
-          { name: '14/01', Pontos: 6 },
+         
           
         ],
     };
 
-    jogadas={login:"ramon", jogo:"3", tempo:"4", tentativas:"7", nrIdAluno:2}
+    jogadas={login:"ramon", jogo:"1", tempo:"4", tentativas:"7", nrIdAluno:1}
+
 
 
     async componentDidMount() {
@@ -37,39 +35,41 @@ class Chart extends PureComponent {
     } 
 
     render() {
-        
-        return (
-            
+        return (        
           <form id="form-id" onSubmit={this.handleSubmit}>
-          <div className="card">
-            <div className="card-header">
-              Ramon do Nascimento Pereira
+             <div className="form-row">
+                {this.state.data.map( game => (
+                  <div className="card" key={game.id}>
+                    <div className="card-header">
+                    {game.nome}
+                    </div>
+                      <div className="card-body">
+                        <h5 className="card-title">Jogo do espaço </h5>
+                        <p className="card-text">Descrição do Jogos de Espaço With supporting text below as a natural lead-in to additional content.</p>
+                      </div> 
+                          
+                          <LineChart
+                        width={500}
+                        height={200}
+                        data={game.jogadas}
+                        margin={{
+                          top: 10, right: 30, left: 0, bottom: 0,
+                        }}
+                      >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="dataCriado" />
+                        <YAxis />
+                        <Tooltip />
+                        <Line connectNulls type="monotone" dataKey="tentativas" stroke="#888500" fill="#000000" />
+                        <Line connectNulls type="monotone" dataKey="tempo" stroke="#8884d8" fill="#8884d8" />
+                      </LineChart>
+
+                      
+                    </div>                                  
+                
+                ))}
             </div>
-            <div className="card-body">
-              <h5 className="card-title">Jogo do espaço </h5>
-              <p className="card-text">Descrição do Jogos de Espaço With supporting text below as a natural lead-in to additional content.</p>
-              
-            </div> 
-            </div>                 
-      <div>
-      <hr></hr>
-        <LineChart
-          width={500}
-          height={200}
-          data={this.state.data}
-          margin={{
-            top: 10, right: 30, left: 0, bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="dtInserida" />
-          <YAxis />
-          <Tooltip />
-          <Line connectNulls type="monotone" dataKey="tentativas" stroke="#8884d8" fill="#8884d8" />
-          <Line connectNulls type="monotone" dataKey="tempo" stroke="#8884d8" fill="#8884d8" />
-        </LineChart>
-      </div>
-  </form>
+          </form>
         )
     }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://mais-que-especial.herokuapp.com"
-    //baseURL: "http://localhost:9001"
+    //baseURL: "https://mais-que-especial.herokuapp.com"
+    baseURL: "http://localhost:9001"
 });
 
 export default api; 
